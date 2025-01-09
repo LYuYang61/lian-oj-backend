934a6ef78e77c8453961056bff27c8dfe30bfcaf
lian <2242727324@qq.com>
2025-01-09 10:41:11 +0800

    oj项目初始化

---
+++ src/main/java/com/lianoj/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.lianoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lianoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
