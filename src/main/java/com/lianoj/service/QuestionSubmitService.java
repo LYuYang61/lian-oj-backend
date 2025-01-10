ae9363aa8eb9a68643a0de733ea797579e4aaf2e
lian <2242727324@qq.com>
2025-01-10 20:39:53 +0800

    题目相关增删改查接口开发

--- src/main/java/com/lianoj/service/QuestionSubmitService.java
+++ src/main/java/com/lianoj/service/QuestionSubmitService.java
@@ -1,7 +1,13 @@
 package com.lianoj.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.lianoj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.lianoj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.lianoj.model.entity.QuestionSubmit;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.lianoj.model.entity.User;
+import com.lianoj.model.vo.QuestionSubmitVO;
 
 /**
 * @author Lenovo
@@ -9,5 +15,39 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2025-01-09 10:51:41
 */
 public interface QuestionSubmitService extends IService<QuestionSubmit> {
+    /**
+     * 题目提交
+     *
+     * @param questionSubmitAddRequest 题目提交信息
+     * @param loginUser
+     * @return
+     */
+    long doQuestionSubmit(QuestionSubmitAddRequest questionSubmitAddRequest, User loginUser);
+
+    /**
+     * 获取查询条件
+     *
+     * @param questionSubmitQueryRequest
+     * @return
+     */
+    QueryWrapper<QuestionSubmit> getQueryWrapper(QuestionSubmitQueryRequest questionSubmitQueryRequest);
+
+    /**
+     * 获取题目封装
+     *
+     * @param questionSubmit
+     * @param loginUser
+     * @return
+     */
+    QuestionSubmitVO getQuestionSubmitVO(QuestionSubmit questionSubmit, User loginUser);
+
+    /**
+     * 分页获取题目封装
+     *
+     * @param questionSubmitPage
+     * @param loginUser
+     * @return
+     */
+    Page<QuestionSubmitVO> getQuestionSubmitVOPage(Page<QuestionSubmit> questionSubmitPage, User loginUser);
 
 }
